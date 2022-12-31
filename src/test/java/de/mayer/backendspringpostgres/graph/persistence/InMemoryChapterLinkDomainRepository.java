6222b39390c1240f73fb93ee129a2b0b72b7622b
Johannes Mayer <johanns.mayer1337@gmail.com>
2022-12-31 17:25:29 +0100

    Fix Test Implementation of ChapterLinkRepository

--- src/test/java/de/mayer/backendspringpostgres/graph/persistence/InMemoryChapterLinkDomainRepository.java
+++ src/test/java/de/mayer/backendspringpostgres/graph/persistence/InMemoryChapterLinkDomainRepository.java
@@ -4,33 +4,26 @@ import de.mayer.backendspringpostgres.adventure.model.Adventure;
 import de.mayer.backendspringpostgres.graph.domainservice.ChapterLinkDomainRepository;
 import de.mayer.backendspringpostgres.graph.model.ChapterLink;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class InMemoryChapterLinkDomainRepository implements ChapterLinkDomainRepository {
-    private HashMap<String, ChapterLink> database;
+    private HashMap<String, Set<ChapterLink>> database;
 
     @Override
     public void save(String adventure, ChapterLink chapterLink) {
         if (this.database == null) {
             this.database = new HashMap<>();
         }
-
-        this.database.put(adventure, chapterLink);
+        var links = this.database.getOrDefault(adventure, new HashSet<>());
+        links.add(chapterLink);
+        this.database.put(adventure, links);
 
     }
 
     @Override
     public Optional<Set<ChapterLink>> findByAdventure(String adventure) {
-        var chapterLinks = database
-                .entrySet()
-                .stream()
-                .filter(entry -> entry.getKey().equals(adventure))
-                .map(Map.Entry::getValue)
-                .collect(Collectors.toSet());
+        var chapterLinks = database.getOrDefault(adventure, new HashSet<>());
         if (chapterLinks.isEmpty())
             return Optional.empty();
         else
