e9248902b500a1de265823286c220982572a25e3
Johannes Mayer <johanns.mayer1337@gmail.com>
2023-01-10 12:06:23 +0100

    Cache: Define invalidated Keys on class level. If no object is put for a class and/or a key, Optional.empty() is returned

--- src/main/java/de/mayer/backendspringpostgres/graph/persistence/Cache.java
+++ src/main/java/de/mayer/backendspringpostgres/graph/persistence/Cache.java
@@ -1,12 +1,13 @@
 package de.mayer.backendspringpostgres.graph.persistence;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Optional;
 
 public class Cache {
     private HashMap<Class<?>, HashMap<String, Object>> cache;
-    private HashSet<String> invalidatedKeys;
+    private HashMap<Class<?>, HashSet<String>> invalidatedKeys;
 
     public <T> void put(String key, T object) {
         if (cache == null) {
@@ -18,20 +19,34 @@ public class Cache {
         }
 
         cache.get(object.getClass()).put(key, object);
+        if (invalidatedKeys != null
+        && invalidatedKeys.containsKey(object.getClass())){
+            invalidatedKeys.get(object.getClass()).remove(key);
+        }
     }
 
     public <T> Optional<T> get(String key, Class<T> aClass) {
         if (invalidatedKeys != null
-                && invalidatedKeys.contains(key)) {
+                && invalidatedKeys.containsKey(aClass)
+                && invalidatedKeys.get(aClass).contains(key)) {
             return Optional.empty();
         }
-        return Optional.of(aClass.cast(cache.get(aClass).get(key)));
+
+        if (cache == null)
+            return Optional.empty();
+
+        if (!cache.containsKey(aClass)){
+            return Optional.empty();
+        }
+
+        return Optional.ofNullable(aClass.cast(
+                cache.get(aClass).getOrDefault(key, null)));
     }
 
-    public void invalidate(String key) {
+    public <T> void invalidate(String key, Class<T> aClass) {
         if (invalidatedKeys == null) {
-            invalidatedKeys = new HashSet<>();
+            invalidatedKeys = new HashMap<>();
         }
-        invalidatedKeys.add(key);
+        invalidatedKeys.put(aClass, new HashSet<>(Collections.singleton(key)));
     }
 }
--- src/test/java/de/mayer/backendspringpostgres/graph/persistence/CacheTest.java
+++ src/test/java/de/mayer/backendspringpostgres/graph/persistence/CacheTest.java
@@ -9,9 +9,7 @@ import java.util.Collections;
 import java.util.Set;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.nullValue;
 import static org.hamcrest.core.Is.is;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 class CacheTest {
 
     @Test
@@ -26,10 +24,11 @@ class CacheTest {
         var graph = new Graph(Set.of(chapter01), Collections.emptySet());
 
         cache.put("Adventure", graph);
+
         var optional = cache.get("Adventure", Graph.class);
 
-        assertThat(optional.isPresent(), is(true));
-        assertThat(optional.get(), is(graph));
+        assertThat("Object can be retrieved", optional.isPresent(), is(true));
+        assertThat("Cached object equals expected Graph", optional.get(), is(graph));
 
     }
 
@@ -46,9 +45,68 @@ class CacheTest {
         var key = "Adventure";
         cache.put(key, graph);
 
-        cache.invalidate(key);
-        assertThat(cache.get(key, Graph.class).isEmpty(), is(true));
+        cache.invalidate(key, Graph.class);
+        assertThat("Object cannot be retrieved", cache.get(key, Graph.class).isEmpty(), is(true));
 
+    }
+
+    @Test
+    @DisplayName("""
+            Given a key is invalidated,
+            when it is put again,
+            it can be retrieved
+            """)
+    void validateKey(){
+        var cache = new Cache();
+        var chapter = new Chapter("Chapter01", 1.0d);
+        var graph = new Graph(Set.of(chapter), Collections.emptySet());
+        var key = "Adventure";
+        cache.invalidate(key, Graph.class);
+
+        cache.put(key, graph);
+        var optional = cache.get("Adventure", Graph.class);
+
+        assertThat("Cached object can be retrieved", optional.isPresent(), is(true));
+        assertThat("Cached object equals expected Graph", optional.get(), is(graph));
+    }
+
+    @Test
+    @DisplayName("""
+            Given no key is neither put nor invalidated,
+            when the key is requested with get,
+            an empty Optional is returned
+            """)
+    void cacheNotInitialised(){
+        assertThat("No object can be retrieved from an empty Cache.",
+                new Cache().get("any key", Graph.class).isEmpty(), is(true));
+    }
+
+    @Test
+    @DisplayName("""
+            Given no key was ever put for the requested Class,
+            when the key is requested with get,
+            an empty Optional is returned
+            """)
+    void classCacheNotInitialised(){
+        var cache = new Cache();
+        cache.put("a key", new Chapter("Chapter 01", 2.0d));
+
+        assertThat("There is no object retrieved for the given Key and Class",
+                cache.get("a key", Graph.class).isEmpty(), is(true));
+    }
+
+    @Test
+    @DisplayName("""
+            Given the requested key was never put,
+            when the key is requested with get,
+            an empty Optional is returned
+            """)
+    void unknownKey(){
+        var cache = new Cache();
+        cache.put("a key", new Chapter("Chapter 01", 1d));
+
+        assertThat("No object can be retrieved for the given key",
+                cache.get("another key", Chapter.class).isEmpty(), is(true));
     }
 
 
